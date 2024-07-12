510dfd7d8db21da30aba16fb039a11065cf4741f
Robin Daugherty <robin@robindaugherty.net>
2024-07-12 14:54:51 +0200

    Content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
+const post = defineCollection({
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
+		description: z.string().optional(),
+		publishedDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		isFeatured: z.boolean().default(false),
+		tags: z.array(z.string()).default([]),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { diy: post, dessert: post };
