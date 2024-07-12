06aa7b3a410850f4642a4ea74e53984783fe748f
Robin Daugherty <robin@robindaugherty.net>
2024-07-12 14:14:00 +0200

    Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sarahmakesstuff.com',
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
 	integrations: [mdx(), sitemap()],
 	vite: {
 		css: {
