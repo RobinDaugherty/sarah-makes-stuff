93f092105a4e9251f7bae19f2f8cd97a83ee9d5d
Robin Daugherty <robin@robindaugherty.net>
2024-07-13 12:12:09 +0200

    Layout and sizing adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,19 @@ import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
+const injectScss = `
+@use "astro-breakpoints/use-breakpoints.scss" as * with (
+$breakpoints: (
+	"xs": "320px",
+	"sm": "640px",
+	"md": "768px",
+	"lg": "1024px",
+	"xl": "1281px",
+	"xxl": "1920px",
+	)
+);
+`;
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sarahmakesstuff.com',
@@ -17,10 +30,7 @@ export default defineConfig({
 				scss: {
 					additionalData(source, filePath) {
 						if (filePath.includes('use-')) return source;
-						return `
-							@use "astro-breakpoints/use-breakpoints.scss" as *;
-							${source}
-						`;
+						return `${injectScss}\n${source}`;
 					},
 				},
 			},
